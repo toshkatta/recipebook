8b1de87dedd1a81016d870fd62af3a51569913be
toshkatta <toshkatta@gmail.com>
2017-03-18 11:18:10 +0200

    add error handler functions

--- app.js
+++ app.js
@@ -18,116 +18,99 @@ let express = require('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let connectionErrorHandler = function (err) {
+    return console.error('error fetching client from pool', err);
+};
+
+let queryErrorHandler = function (err) {
+    return console.error('error running query', err);
+};
+
+let clientErrorHandler = function (err, client) {
+    // if an error is encountered by a client while it sits idle in the pool
+    // the pool itself will emit an error event with both the error and
+    // the client which emitted the original error
+    // this is a rare occurrence but can happen if there is a network partition
+    // between your application and the database, the database restarts, etc.
+    // and so you might want to handle it and at least log it out
+    console.error('idle client error', err.message, err.stack)
+    res.status(500).send({ error: err });
+};
+
 app.get('/', function (req, res) {
     pool.connect(function (err, client, done) {
         if (err) {
-            return console.error('error fetching client from pool', err);
+            connectionErrorHandler(err);
         }
 
         client.query('SELECT * FROM recipes', function (err, result) {
             if (err) {
-                return console.error('error running query', err);
+                queryErrorHandler(err);
             }
 
-            res.status(200).send({data: result.rows});
+            res.status(200).send({ data: result.rows });
             done();
         });
     });
 
-    pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network partition
-        // between your application and the database, the database restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-        res.status(500).send({error: err});
-    })
+    pool.on('error', clientErrorHandler(err, client));
 });
 
 app.post('/add', function (req, res) {
     pool.connect(function (err, client, done) {
         if (err) {
-            return console.error('error fetching client from pool', err);
+            connectionErrorHandler(err);
         }
 
         client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)', [req.body.name, req.body.ingredients, req.body.directions], function (err, result) {
             if (err) {
-                return console.error('error running query', err);
+                queryErrorHandler(err);
             }
 
-            res.status(200).send({data: result.rows});
+            res.status(200).send({ data: result.rows });
             done();
         });
     });
 
-    pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network partition
-        // between your application and the database, the database restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-        res.status(500).send({error: err});
-    })
+    pool.on('error', clientErrorHandler(err, client));
 });
 
 app.delete('/delete/:id', function (req, res) {
     pool.connect(function (err, client, done) {
         if (err) {
-            return console.error('error fetching client from pool', err);
+            connectionErrorHandler(err);
         }
 
         client.query('DELETE FROM recipes WHERE id = $1', [req.params.id], function (err, result) {
             if (err) {
-                return console.error('error running query', err);
+                queryErrorHandler(err);
             }
 
-            res.status(200).send({data: result.rows});
+            res.status(200).send({ data: result.rows });
             done();
         });
     });
 
-    pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network partition
-        // between your application and the database, the database restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-        res.status(500).send({error: err});
-    })
+    pool.on('error', clientErrorHandler(err, client));
 });
 
 app.put('/edit', function (req, res) {
     pool.connect(function (err, client, done) {
         if (err) {
-            return console.error('error fetching client from pool', err);
+            connectionErrorHandler(err);
         }
 
         client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id = $4', [req.body.name, req.body.ingredients, req.body.directions, req.body.id], function (err, result) {
             if (err) {
-                return console.error('error running query', err);
+                queryErrorHandler(err);
             }
 
-            res.status(200).send({data: result.rows});
+            res.status(200).send({ data: result.rows });
             done();
         });
     });
 
-    pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network partition
-        // between your application and the database, the database restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-        res.status(500).send({error: err});
-    })
+    pool.on('error', clientErrorHandler(err, client));
 });
 
 app.listen(3006, function () {
